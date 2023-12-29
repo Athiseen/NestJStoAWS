eed0f98b56524b46b96b0c1a407faf752c7ce855
Athiseen <longkatana555@gmail.com>
2023-12-29 10:02:20 +0700

    fifth commit: try node main.js

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.port || 3000;
-  app.listen(port, () => {
-    console.log('Sever console log.');
-  });
+  app.listen(port);
 }
 bootstrap();
