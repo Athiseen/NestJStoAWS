0809a97ad6d4b32670117a96a29a77d3d3a4feef
Athiseen <longkatana555@gmail.com>
2023-12-29 09:15:08 +0700

    second commit: config package.json

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.port || 3000;
+  app.listen(port, () => {
+    console.log('Sever console log.');
+  });
 }
 bootstrap();
